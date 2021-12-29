a7b4bd770f43210b874bf253c62828a2daa118ed
Fadi Idoudi <fadi.idoudi@outlook.com>
2021-12-29 23:03:14 +0100

    grpc client settings

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -18,6 +18,12 @@ namespace CommandsService.Profiles
             CreateMap<Command, CommandReadDto>();
             CreateMap<PlatformPublishedDto, Platform>()
                 .ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.Id));
+            //Grpc classes are auto generated
+            //if you have error here build the project first
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.PlatformId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands, opt => opt.Ignore());
         }
     }
 }
---
+++ CommandsService/SyncDataServices/Grpc/PlatformDataClient.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using CommandsService.Models;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsService.SyncDataServices.Grpc
+{
+    public class PlatformDataClient : IPlatformDataClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public PlatformDataClient(IConfiguration configuration, IMapper mapper)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+        public IEnumerable<Platform> ReturnAllPlatforms()
+        {
+            Console.WriteLine($"--> Calling Grpc Service {_configuration["GrpcPlatform"]}");
+            var grpcChannel = GrpcChannel.ForAddress(_configuration["GrpcPlatform"]);
+            var grpcClient = new GrpcPlatform.GrpcPlatformClient(grpcChannel);
+            //GetAllRequest() from platforms.proto
+            var request = new GetAllRequest();
+
+            try
+            {
+                var reply = grpcClient.GetAllPlatforms(request);
+                return _mapper.Map<IEnumerable<Platform>>(reply.Platform);
+            } catch (Exception e)
+            {
+                Console.WriteLine($"--> Could Not call Grpc Server : {e.Message}");
+                return null;
+            }
+        }
+    }
+}
